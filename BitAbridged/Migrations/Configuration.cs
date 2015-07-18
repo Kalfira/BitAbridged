71862c4db921a30f7c17edf2466a83cc7a82bb3a
Kalfira <zdegner@gmail.com>
2015-07-18 15:56:17 -0500

    Init

---
+++ BitAbridged/Migrations/Configuration.cs
@@ -0,0 +1,42 @@
+using BitAbridged.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BitAbridged.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BitAbridged.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(BitAbridged.Models.ApplicationDbContext context)
+        {
+            UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+            UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+
+            RoleStore<Role> roleStore = new RoleStore<Role>(context);
+            RoleManager<Role> roleManager = new RoleManager<Role>(roleStore);
+
+            if (!roleManager.RoleExists("Admin"))
+                roleManager.Create(new Role { Name = "Admin" });
+
+            if (!roleManager.RoleExists("User"))
+                roleManager.Create(new Role { Name = "User" });
+            ApplicationUser zane = userManager.FindByName("zdegner@gmail.com");
+
+            zane = new ApplicationUser
+            {
+                Id = "1",
+                UserName = "admin",
+                Email = "zdegner@gmail.com"
+            };
+            userManager.Create(zane, "12341234");
+            userManager.AddToRole(zane.Id, "Admin");
+            context.SaveChanges();
+        }
+    }
+}
---
+++ BitAbridged/obj/Debug/Package/PackageTmp/app/app.js
@@ -0,0 +1,24 @@
+﻿(function() {
+    'use strict';
+    angular.module('BitAbridged', ['ngRoute']).config(Config);
+
+    function Config($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.when('/', {
+            templateUrl: 'app/views/welcome.html',
+            controller: 'WelcomeController',
+            controllerAs: 'vm'
+        }).when('/demo', {
+            templateUrl: 'app/views/demo.html',
+            controller: 'DemoController',
+            controllerAs: 'vm'
+        }).when('/login', {
+            templateUrl: '/app/views/login.html',
+            controller: 'LoginController'
+            })
+            .otherwise({
+            redirectTo: '/'
+            });
+        
+    }
+})();
